d900c462d45dd66f25a162a40273b4c14c072e12
Yugen <yugen.tanaka@icloud.com>
2025-08-14 18:34:32 +0900

    Initial Commit

---
+++ PomoTimerDev/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PomoTimerDev
+//
+//  Created by 田中悠元 on 2025/08/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PomoTimerDev/PomoTimerDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoTimerDevApp.swift
+//  PomoTimerDev
+//
+//  Created by 田中悠元 on 2025/08/14.
+//
+
+import SwiftUI
+
+@main
+struct PomoTimerDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
